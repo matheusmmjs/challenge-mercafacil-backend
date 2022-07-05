894962a73cf3b33e6ee2d8a142bddd0c3af2c228
Matheus Santos <matheusmatmac@hotmail.com>
2022-07-05 16:15:45 -0300

    fix: Remove update dto

--- src/contacts-varejao/dto/update-contacts-varejao.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateContactsVarejaoDto } from './create-contacts-varejao.dto';
-
-export class UpdateContactsVarejaoDto extends PartialType(
-  CreateContactsVarejaoDto,
-) {}
